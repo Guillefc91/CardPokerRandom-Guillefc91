014a61785491816630bae6112988a2927b571f3b
Guillermo Fernández Coscollano <167807897+Guillefc91@users.noreply.github.com>
2024-08-12 13:09:33 +0000

    Generador de cartas con .onload,settimeinteval y button

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let suits = ["♦", "♥", "♠", "♣"];
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+
+  document.querySelector("#changeCard").addEventListener("click", () => {
+    generateCard();
+  });
 };
+
+setInterval(generateCard, 10000);
+
+function generateCard() {
+  const randomNumbers = Math.floor(Math.random() * numbers.length);
+  const randomSuits = Math.floor(Math.random() * suits.length);
+
+  const numberCard = document.querySelector("#centerCard");
+  numberCard.innerHTML = numbers[randomNumbers];
+  const suitCardTop = document.querySelector("#cornerTop");
+  suitCardTop.innerHTML = suits[randomSuits];
+  const suitCardBottom = document.querySelector("#cornerBottom");
+  suitCardBottom.innerHTML = suits[randomSuits];
+
+  if (suits[randomSuits] == "♦" || suits[randomSuits] == "♥") {
+    document.querySelector(".card").style.color = "red";
+  } else {
+    document.querySelector(".card").style.color = "black";
+  }
+}
